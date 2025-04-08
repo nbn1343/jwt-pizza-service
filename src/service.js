b5260e9fd1ba2e3b53890a6671709c010af906fc
Nathan Neilson <nbn1343@gmail.com>
2025-04-07 22:21:25 -0600

    CORS

--- src/service.js
+++ src/service.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(setAuthUser);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
   next();
